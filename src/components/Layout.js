12fe73e2874ca6d8495d0c8b212859ff212c72b8
alexsantostx94 <58117138+alexsantostx94@users.noreply.github.com>
2020-08-07 12:47:26 -0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,7 +19,8 @@ export default class Body extends React.Component {
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"/>
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-                    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>                            
+                    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+                    <link rel="manifest" href="/images/site.webmanifest"></link>
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.color_scheme', null) + ' accent-' + _.get(this.props, 'pageContext.site.siteMetadata.accent_color', null)}>
                   <Header {...this.props} />
